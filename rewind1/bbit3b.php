44651f3dd648631c60c63822e1738c4efaef7f0d
June Katunge <june.katunge@strathmore.edu>
2018-05-04 09:54:53 +0300

    ghnjmk,

--- rewind1/bbit3b.php
+++ rewind1/bbit3b.php
@@ -21,7 +21,7 @@
   6. Submit the github url on google classroom (Check Quiz 1 3B)
  */
 
- $rewind1 = " 'Joy is the serious business of heaven.'.LEWIS,CLIVE STAPLES.1964-01-01.https://bit.ly/2HwPJd6
+ $rewind1 = "'Joy is the serious business of heaven.'.LEWIS,CLIVE STAPLES.1964-01-01.https://bit.ly/2HwPJd6
 |'We were not meant to be somebody--we were meant to know Somebody'.PIPER,JOHN STEPHEN.2011-07-17.https://bit.ly/2r31InJ|'He who sings prays twice.'.Hipponensis,Aurelius Augustinus.430-02-30.https://bit.ly/2JwSHuH
 |'The task of the modern educator is not to cut down jungles but to irrigate deserts.'.LEWIS,CLIVE STAPLES.1943-09-23.https://bit.ly/2HwPJd6
 |'There is not one blade of grass, there is no color in this world that is not intended to make us rejoice.'.Calvin,John C.1530-10-09.https://www.brainyquote.com/authors/john_calvin|
@@ -32,17 +32,50 @@
 |'It is better to lose your life than to waste it.'.PIPER,JOHN STEPHEN.2000-05-33.https://bit.ly/2r31InJ|
 'It is not opinions that man needs: it is TRUTH...'.Bonar Horatius B.1885-02-12.https://www.goodreads.com/author/quotes/133605.Horatius_Bonar|
 'Nothing could be more irrational than the idea that something comes from nothing.'SPROUL,CHARLES ROBERT.2006-03-23.https://bit.ly/2HQ4TJV
-";
+|'He is no fool who gives what he cannot keep to gain that which he cannot lose.'.Elliot,James Phillip.1944-07-26.https://www.brainyquote.com/authors/jim_elliot";
 //to break string to array
-$rewind1_array= explode('|',$rewind1);
+$str = str_replace("| ", ",", $rewind1);
+$rewind1_array= explode('|',$str);
 $rewind1_array2=[];
+$rewind1_array3=[];
 
-//print_r($rewind1_array);
-foreach ($rewind1_array as $result1){
- $rewind1_array2['part1']= explode(',',$result1);
+//echo json_encode($rewind1_array);
+
+
+//foreach ($rewind1_array as &$value) {
+// $arr = array();
+// foreach (explode(', ', $value) as $el) {
+//  $ret = explode(':', $el);
+//  $arr[$ret[0]] = $ret[1];
+// }
+// $value = $arr;
+// array_push($rewind1_array3,$value);
+//}
+
+foreach ($rewind1_array as $result){
+
+ $rewind1_array2[] = str_replace(".", " ", $result);
+// print_r($rewind1_array2);
+// $ret = explode('.', $result);
+ foreach ($rewind1_array2 as $res){
+  print_r($res .'<br/>');
+ }
 
 }
-echo json_encode($rewind1_array2);
+
+//print_r($rewind1_array);
+//foreach ($rewind1_array as $result1) {
+// $rewind1_array1= explode('|',$result1);
+// array_push($rewind1_array2, $rewind1_array1);
+
+// foreach ($rewind1_array1 as $result2){
+//  $rewind1_array2= explode('|',$result2);
+//  array_push($rewind1_array3,$result2);
+//  print_r($result2);
+// }
+
+//}
+//echo json_encode($rewind1_array3);
 
 
 //print_r($myJSON);
